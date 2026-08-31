7aabade916a15ee56867769d3f94fa2b53284637
dhruvparmar25 <parmardhruv034@gmail.com>
2026-08-31 13:34:44 +0530

    be/first-day

---
+++ 01-python-basics/01_variables_datatypes.py
@@ -0,0 +1,17 @@
+#variables
+name="Dhruv"
+age=24
+experience=0.5
+is_developer=True
+
+#output
+print(name)
+print(age)
+print(experience)
+print(is_developer)
+
+#type
+print(type(name))
+print(type(age))
+print(type(experience))
+print(type(is_developer))
